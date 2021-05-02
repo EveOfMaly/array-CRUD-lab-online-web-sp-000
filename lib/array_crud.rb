5bf248eaad3716b4d8df08afcba55b85f2df1c37
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-02 02:27:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,13 +38,12 @@ end
    array = ["wow", "I", "am", "really", "learning"]
     element = "arrays!"
     index_number = 1
-puts "create_an_empty_arrputs create_an_empty_array
-puts "create an arrputs create_an_array
+puts "create_an_empty_arrputs create_an_empty_array"
+puts create an arrputs create_an_array
 puts "add element to end of the array"
 puts add_element_to_end_of_array(array, element)
 puts "add element to the start of the array"
 puts add_element_to_start_of_array(array, element)
-
 puts "remove element from end of the array"
 puts remove_element_from_end_of_array(array)
  
@@ -53,7 +52,8 @@ puts remove_element_from_start_of_array(array)
  
 puts "retrieve element from index 1"
 puts retrieve_element_from_index(array, index_number)
-puts "retrieve first element"putst retrieve_first_element_from_array(array)
+puts "retrieve first element"
+putt retrieve_first_element_from_array(array)
 puts "retrieve last element"
 puts retrieve_last_element_from_array(array)
 
