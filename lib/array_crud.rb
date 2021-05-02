58963e9e67eb02fd646f728194e8c421b248ed05
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-02 02:26:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,25 +38,22 @@ end
    array = ["wow", "I", "am", "really", "learning"]
     element = "arrays!"
     index_number = 1
-puts "create_an_empty_array"
-print create_an_empty_array
-puts "create an array"
-print create_an_array
+puts "create_an_empty_arrputs create_an_empty_array
+puts "create an arrputs create_an_array
 puts "add element to end of the array"
-print add_element_to_end_of_array(array, element)
+puts add_element_to_end_of_array(array, element)
 puts "add element to the start of the array"
-print add_element_to_start_of_array(array, element)
+puts add_element_to_start_of_array(array, element)
 
 puts "remove element from end of the array"
-print remove_element_from_end_of_array(array)
+puts remove_element_from_end_of_array(array)
  
 puts "remove element from start of the array"
-print remove_element_from_start_of_array(array)
+puts remove_element_from_start_of_array(array)
  
 puts "retrieve element from index 1"
-print retrieve_element_from_index(array, index_number)
-puts "retrieve first element"
-print retrieve_first_element_from_array(array)
+puts retrieve_element_from_index(array, index_number)
+puts "retrieve first element"putst retrieve_first_element_from_array(array)
 puts "retrieve last element"
-print retrieve_last_element_from_array(array)
+puts retrieve_last_element_from_array(array)
 
